df07326340999d4848a8a35863507d2def3dec2f
John-Zelade <https://johnzeladehigo@gmail.com>
2023-02-10 10:24:49 +0800

    sa

---
+++ react-app1.1/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './sing-up/signup.css';
+import Signup from './sing-up/signup'
+
+class App  extends Component {
+
+    render() { 
+        return (
+            <div className='App'>
+                <Signup />
+            </div>
+        );
+    }
+}
+ 
+export default App ;
